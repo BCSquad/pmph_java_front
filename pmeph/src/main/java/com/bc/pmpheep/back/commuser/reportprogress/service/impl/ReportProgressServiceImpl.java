23b20140da08b30966836fec6112e2f2132555bf
nyz526 <nyz526@163.com>
2017-12-06 18:15:00 +0800

    申报进度，书籍列表代码提交

--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/reportprogress/service/impl/ReportProgressServiceImpl.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/reportprogress/service/impl/ReportProgressServiceImpl.java
@@ -1,11 +1,14 @@
 package com.bc.pmpheep.back.commuser.reportprogress.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bc.pmpheep.back.commuser.reportprogress.bean.Declaration;
 import com.bc.pmpheep.back.commuser.reportprogress.bean.TextBookCheckVO;
+import com.bc.pmpheep.back.commuser.reportprogress.bean.UserMessageVO;
 import com.bc.pmpheep.back.commuser.reportprogress.dao.ReportProgressDao;
 import com.bc.pmpheep.back.commuser.reportprogress.service.ReportProgressService;
 import com.bc.pmpheep.back.util.ObjectUtil;
@@ -30,7 +33,7 @@ import com.bc.pmpheep.service.exception.CheckedServiceException;
  * @审核人 ：
  * </pre>
  */
-@Service
+@Service("com.bc.pmpheep.back.commuser.reportprogress.service.impl.ReportProgressServiceImpl")
 public class ReportProgressServiceImpl implements ReportProgressService {
     @Autowired
     ReportProgressDao reportProgressDao;
@@ -45,10 +48,10 @@ public class ReportProgressServiceImpl implements ReportProgressService {
         if (ObjectUtil.notNull(textBookCheckVO)) {
             // 审核进度 0=未提交/1=已提交/2=被退回/3=通过
             Integer online_progress_1 = 1;
-            Integer online_progress_2 = 2;
+            Integer online_progress_3 = 3;
             if (online_progress_1.intValue() == textBookCheckVO.getOnlineProgress().intValue()
-                || online_progress_2 == textBookCheckVO.getOnlineProgress().intValue()) {
-                textBookCheckVO.setOnlineSubmit(1);// 已提交
+                || online_progress_3 == textBookCheckVO.getOnlineProgress().intValue()) {
+                textBookCheckVO.setOnlineSubmit(online_progress_1);// 已提交
             }
         }
         return textBookCheckVO;
@@ -62,6 +65,39 @@ public class ReportProgressServiceImpl implements ReportProgressService {
                                               CheckedExceptionResult.NULL_PARAM, "参数为空");
         }
         return reportProgressDao.getTextBookCheckResult(userId, materialId);
+    }
+
+    @Override
+    public List<UserMessageVO> getUserMessageByMaterialId(Long userId, Long materialId)
+    throws Exception {
+        if (ObjectUtil.isNull(userId) || ObjectUtil.isNull(materialId)) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.MATERIAL,
+                                              CheckedExceptionResult.NULL_PARAM, "参数为空");
+        }
+        List<UserMessageVO> userMessageList = new ArrayList<UserMessageVO>(4);
+        List<UserMessageVO> lists =
+        reportProgressDao.getUserMessageByMaterialId(userId, materialId);// 取近4条消息
+        if (lists.size() == 4) {
+            userMessageList.addAll(lists);
+        } else {
+            Declaration declaration = this.getDeclarationByMaterialIdAndUserId(userId, materialId);
+            if (ObjectUtil.notNull(declaration)) {
+                userMessageList.add(new UserMessageVO(materialId, "您的申报已提交，请耐心等待审核...",
+                                                      declaration.getGmtCreate()));
+            }
+            userMessageList.addAll(lists);
+        }
+        return userMessageList;
+    }
+
+    @Override
+    public Declaration getDeclarationByMaterialIdAndUserId(Long userId, Long materialId)
+    throws Exception {
+        if (ObjectUtil.isNull(userId) || ObjectUtil.isNull(materialId)) {
+            throw new CheckedServiceException(CheckedExceptionBusiness.MATERIAL,
+                                              CheckedExceptionResult.NULL_PARAM, "参数为空");
+        }
+        return reportProgressDao.getDeclarationByMaterialIdAndUserId(userId, materialId);
     }
 
 }
