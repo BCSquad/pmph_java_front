38ebd976f453063a3db91c1b4ebbd243d5cbb70e
lideveloper <437305629@qq.com>
2017-12-18 18:05:05 +0800

    解决报错

--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/myfriend/controller/MyFriendController.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/myfriend/controller/MyFriendController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.bc.pmpheep.back.commuser.user.bean.CommuserWriterUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -12,28 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.bc.pmpheep.back.authadmin.usermanage.bean.WriterUser;
-import com.bc.pmpheep.back.commuser.myfriend.bean.WriterFriendVO;
 import com.bc.pmpheep.back.commuser.myfriend.service.MyFriendService;
-import com.bc.pmpheep.back.util.CollectionUtil;
-import com.bc.pmpheep.back.util.RouteUtil;
 import com.bc.pmpheep.service.exception.CheckedServiceException;
 
 /**
- * 
  * <pre>
  * 功能描述：好友 控制器
  * 使用示范：
- * 
- * 
+ *
+ *
  * @author (作者) nyz
- * 
+ *
  * @since (该版本支持的JDK版本) ：JDK 1.6或以上
  * @version (版本) 1.0
  * @date (开发日期) 2017-11-29
- * @modify (最后修改时间) 
- * @修改人 ：nyz 
+ * @modify (最后修改时间)
+ * @修改人 ：nyz
  * @审核人 ：
  * </pre>
  */
@@ -45,7 +40,6 @@ public class MyFriendController extends com.bc.pmpheep.general.controller.BaseCo
     MyFriendService myFriendService;
 
     /**
-     * 
      * <pre>
      * 功能描述：获取当前用户好友列表
      * 使用示范：
@@ -63,43 +57,44 @@ public class MyFriendController extends com.bc.pmpheep.general.controller.BaseCo
 
         // 获取用户
         Map<String, Object> writerUserMap = this.getUserInfo();
-        WriterUser writerUser = new WriterUser();
+        CommuserWriterUser writerUser = new CommuserWriterUser();
         writerUser.setId(Long.parseLong(writerUserMap.get("id").toString()));
         writerUser.setId(12180L);
         String pageUrl = "commuser/myfriend/myFriend";
         try {
-        	int startrow=0;
-            List<Map<String, Object>> listFriends = myFriendService.listMyFriend(writerUser,startrow);
+            int startrow = 0;
+            List<Map<String, Object>> listFriends = myFriendService.listMyFriend(writerUser, startrow);
             model.setViewName(pageUrl);
             model.addObject("row", startrow);
             model.addObject("id", writerUser.getId());
-            model.addObject("more",listFriends.size());
+            model.addObject("more", listFriends.size());
             model.addObject("listFriends", listFriends);
         } catch (CheckedServiceException e) {
             throw new CheckedServiceException(e.getBusiness(), e.getResult(), e.getMessage(),
-                                              pageUrl);
+                    pageUrl);
         }
         return model;
     }
-    
+
     /**
      * 加载更多
+     *
      * @param request
      * @return
      * @throws Exception
      */
     @RequestMapping("more")
     @ResponseBody
-    public List<Map<String, Object>> more(HttpServletRequest request) throws Exception{
-    	int row=Integer.parseInt(request.getParameter("row"));
-    	String id=request.getParameter("id");
-    	WriterUser writerUser = new WriterUser();
+    public List<Map<String, Object>> more(HttpServletRequest request) throws Exception {
+        int row = Integer.parseInt(request.getParameter("row"));
+        String id = request.getParameter("id");
+        CommuserWriterUser writerUser = new CommuserWriterUser();
         writerUser.setId(Long.parseLong(id.toString()));
-        List<Map<String, Object>> listFriends = myFriendService.listMyFriend(writerUser,row+15);
-        for(int i=0;i<listFriends.size();i++){
-        	listFriends.get(i).put("row",row+15);
-        	listFriends.get(i).put("queryid",id);
+        List<Map<String, Object>> listFriends = myFriendService.listMyFriend(writerUser, row + 15);
+        for (int i = 0; i < listFriends.size(); i++) {
+            listFriends.get(i).put("row", row + 15);
+            listFriends.get(i).put("queryid", id);
         }
-    	return listFriends;
+        return listFriends;
     }
 }
