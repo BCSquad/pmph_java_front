c09ec7f6dec0fa52d8bc37dbba5143bf9563279d
xiaochengyan <985814585@qq.com>
2017-12-25 19:00:59 +0800

    点赞、评论

--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/cms/controller/ArticleDetailController.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/cms/controller/ArticleDetailController.java
@@ -68,14 +68,77 @@ public class ArticleDetailController extends BaseController {
 		// 相关文章换一换
 		List<Map<String, Object>> eMap = articleDetailService
 				.queryRecommendByE(0);
+		
+		
+		PageParameter<Map<String, Object>> pageParameter=new PageParameter<Map<String, Object>>(1,2);
+		pageParameter.setParameter(map1);
+		PageResult<Map<String, Object>> listCom=articleDetailService.queryComment(pageParameter);
+		mv.addObject("listCom", listCom);
+		
+		 List<Map<String, Object>> listArtSix = articleDetailService.queryArticleSix();
+		 //点赞
+		 Map<String, Object> user=getUserInfo();
+			if(user!=null){
+				Map<String, Object> zmap=new HashMap<String, Object>();
+				zmap.put("content_id", wid);
+				zmap.put("writer_id", user.get("id"));
+				List<Map<String, Object>> list=articleDetailService.queryLikes(zmap);
+				Map<String, Object> amap=articleDetailService.queryDedaultFavorite(user.get("id").toString());
+				if(amap==null){
+					mv.addObject("mark", "no");
+				}else{
+					int x=articleDetailService.queryMark(wid,amap.get("id").toString(),user.get("id").toString());
+					if(x>0){
+						mv.addObject("mark", "yes");	
+					}else{
+						mv.addObject("mark", "no");	
+					}
+				}
+				if(list.size()>0){
+					mv.addObject("flag", "yes");
+				}else{
+					mv.addObject("flag","no");
+				}
+			}else{
+				mv.addObject("flag","no");
+				//mv.addObject("mark", "no");
+			}
+		mv.addObject("wid", wid); 
 		mv.addObject("map", map);
 		mv.addObject("UEContent", UEContent);
 		mv.addObject("listArt", listArt);
 		mv.addObject("Art", Art);
 		mv.addObject("numArt", numArt);
 		mv.addObject("eMap", eMap);
+		mv.addObject("listArtSix", listArtSix);
 		mv.setViewName("/commuser/cms/articledetail");
 		return mv;
+	}
+	
+	/**
+	 * 分页的具体实现
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("changepage")
+	@ResponseBody
+	public PageResult<Map<String, Object>> changepage(HttpServletRequest request){
+		int pageNumber=Integer.parseInt(request.getParameter("pageNumber"));
+		int allppage=Integer.parseInt(request.getParameter("allppage"));
+//		String id=request.getParameter("id");
+		String wid = "10";
+		PageParameter<Map<String, Object>> pageParameter=new PageParameter<Map<String, Object>>(pageNumber,allppage);
+		Map<String, Object> wMap=new HashMap<String, Object>();
+		wMap.put("id", wid);
+		pageParameter.setParameter(wMap);
+		PageResult<Map<String, Object>> listCom=articleDetailService.queryComment(pageParameter);
+		// mongoDB查询评论
+		for (Map<String, Object> pmap : listCom.getRows()) {
+			String comment = messageService.get((String) pmap.get("mid")).getContent();
+			pmap.put("mid", comment);
+		}
+		
+		return listCom;
 	}
 
 	/**
@@ -108,17 +171,68 @@ public class ArticleDetailController extends BaseController {
 	@ResponseBody
 	public Map<String, Object> insertComment(HttpServletRequest request){
 		Map<String, Object> map=new HashMap<String, Object>();
+		String wid = "10";
 		String content=request.getParameter("content");
-		//map.put("score", request.getParameter("score"));
+		
 		Map<String, Object> user=getUserInfo();
-		map.put("parent_id", 0); //上级id
+		map.put("score", request.getParameter("score"));
+		map.put("parent_id", wid); //上级id
 		map.put("category_id",0); //内容类型 0评论
 		map.put("author_type",2); //作者类型
 		map.put("author_id",user.get("id")); //作者id
 		map.put("is_staging",1); //提交
 		map.put("path",0); //根路径
+		
 		Map<String, Object> flagMap = articleDetailService.insertWriteArticle(map,content);
 		return flagMap;
+	}
+	
+	/**
+	 * 根据增加点赞数
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("addlikes")
+	@ResponseBody
+	public Map<String, Object> addlikes(HttpServletRequest request){
+		/*String id=request.getParameter("id");*/
+		String wid = "10";
+		Map<String, Object> pmap=articleDetailService.queryRead(wid);
+		int likes=Integer.parseInt(pmap.get("likes").toString());
+		Map<String, Object> user=getUserInfo();
+		Map<String, Object> zMap=new HashMap<String, Object>();
+		zMap.put("content_id", wid);
+		zMap.put("writer_id", user.get("id"));
+		List<Map<String, Object>> list=articleDetailService.queryLikes(zMap);
+		Map<String, Object> map=new HashMap<String, Object>();
+		if(list.size()>0){
+			Map<String, Object> iMap=new HashMap<String, Object>();
+			iMap.put("id", list.get(0).get("id"));
+			iMap.put("content_id", wid);
+			iMap.put("likes", likes-1);
+			iMap.put("flag", "del");
+			map=articleDetailService.addlikes(iMap);
+		}else{
+			Map<String, Object> iMap=new HashMap<String, Object>();
+			iMap.put("writer_id", user.get("id"));
+			iMap.put("content_id", wid);
+			iMap.put("likes", likes+1);
+			iMap.put("flag", "add");
+	 		map=articleDetailService.addlikes(iMap);
+		}
+	    return map;
+	}
+	
+	//添加收藏
+	@RequestMapping("/addmark")
+	@ResponseBody
+	public Map<String, Object> addMark(HttpServletRequest request){
+		Map<String,Object> map=new HashMap<>();
+		long wid=Long.valueOf(request.getParameter("wid"));
+		Map<String,Object> userMap=getUserInfo();
+		long writer_id=Long.valueOf(userMap.get("id").toString());
+		map=articleDetailService.inserMark(wid,writer_id);
+		return map;
 	}
 	
 
--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/cms/service/ArticleDetailService.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/cms/service/ArticleDetailService.java
@@ -15,6 +15,13 @@ import com.bc.pmpheep.back.plugin.PageResult;
 public interface ArticleDetailService {
 	
 	/**
+	 * 查询读书详情页的信息
+	 * @param id
+	 * @return map
+	 */
+	Map<String, Object> queryRead(String id);
+	
+	/**
 	 * 查询医学随笔详情页的信息(文章)
 	 * @param id
 	 * @return map
@@ -51,5 +58,53 @@ public interface ArticleDetailService {
 	 * @return List<Map<String, Object>>
 	 */
 	List<Map<String, Object>> queryRecommendByE(int num);
+	
+	/**
+	 * 查询书籍相关评论
+	 * @param id
+	 * @return
+	 */
+	PageResult<Map<String, Object>> queryComment(PageParameter<Map<String, Object>> pageParameter);
+	
+	/**
+	 * @Description: 查询猜你喜欢
+	 * @return List<Map<String, Object>>
+	 */
+	List<Map<String, Object>> queryArticleSix();
+	
+	/**
+	 * 根据书籍ID增加点赞数
+	 * @param id
+	 */
+	Map<String, Object> addlikes(Map<String, Object> map);
+	/**
+	 * 根据book_id和writer_id查询是否点赞
+	 * @param map
+	 * @return
+	 */
+	List<Map<String, Object>> queryLikes(Map<String, Object> map);
+	
+	/**
+	 * 查询是否有收藏夹
+	 * @param id
+	 * @return
+	 */
+	Map<String, Object> queryDedaultFavorite(String id);
+	
+	/**
+	 * 查询默认收藏夹是否收藏本书
+	 * @param bookId
+	 * @param favoriteId
+	 * @param writerId
+	 * @return
+	 */
+	int queryMark(String wid,String favorite_id,String writer_id);
+	
+	/**添加收藏
+	 * @param bookId   书籍id
+	 * @param writerId  作家用户id
+	 * @return
+	 */
+	Map<String, Object> inserMark(long wid,long writer_id);
 	
 }
