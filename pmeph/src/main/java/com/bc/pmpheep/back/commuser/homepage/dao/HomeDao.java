432e364adc9883bbc7977c79c96304b48f5c6708
lideveloper <437305629@qq.com>
2018-01-31 14:08:57 +0800

    添加广告

--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/homepage/dao/HomeDao.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/homepage/dao/HomeDao.java
@@ -6,57 +6,80 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Param;
 
 
-
 /**
- * 
  * @author xieming
  * @Description: 首页的DAO层
  */
 public interface HomeDao {
-	
-	/**
-	 * @Description: 查询三条最新公告
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryDocument(@Param("id") String id);
-	/**
-	 * @Description: 查询前五条信息快报 
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryNotice();
-	/**
-	 * @Description: 查询最新四条医学随笔
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryArticle(@Param("endrow") int endrow);
-	/**
-	 * 查询作者
-	 * @return List<Map<String, Object>>
-	 */
-	List<Map<String, Object>> queryAuthor();
-	/**
-	 * @Description: 查询四个点击人数最多的随笔的作者
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryComment();
-	/**
-	 * @Description: 查询某分类下的书
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryBook(Map<String, Object> map);
-	/**
-	 * @Description: 查询销量最高的6本书
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> querySale(@Param("type")int type);
-	/**
-	 * @Description: 查询第一级下的子类
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryBookType(@Param("parent_id")int parent_id);
-	/**
-	 * @Description: 查询热门标签
-	 * @return List<HomepageDocument>
-	 */
-	List<Map<String, Object>> queryLabel(String typepath);
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询三条最新公告
+     */
+    List<Map<String, Object>> queryDocument(@Param("id") String id);
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询前五条信息快报
+     */
+    List<Map<String, Object>> queryNotice();
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询最新四条医学随笔
+     */
+    List<Map<String, Object>> queryArticle(@Param("endrow") int endrow);
+
+    /**
+     * 查询作者
+     *
+     * @return List<Map<String, Object>>
+     */
+    List<Map<String, Object>> queryAuthor();
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询四个点击人数最多的随笔的作者
+     */
+    List<Map<String, Object>> queryComment();
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询某分类下的书
+     */
+    List<Map<String, Object>> queryBook(Map<String, Object> map);
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询销量最高的6本书
+     */
+    List<Map<String, Object>> querySale(@Param("type") int type);
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询第一级下的子类
+     */
+    List<Map<String, Object>> queryBookType(@Param("parent_id") int parent_id);
+
+    /**
+     * @return List<HomepageDocument>
+     * @Description: 查询热门标签
+     */
+    List<Map<String, Object>> queryLabel(String typepath);
+
+    /**
+     * 获取页面广告，用名称匹配
+     *
+     * @param adName
+     * @return
+     */
+    public Map<String, Object> getPageAdInfo(@Param("adName") String adName);
+
+    /**
+     * 获取页面广告，用名称匹配
+     *
+     * @param adName
+     * @return
+     */
+    public List<Map<String, Object>> getPageAdDetail(@Param("adName") String adName);
 }
