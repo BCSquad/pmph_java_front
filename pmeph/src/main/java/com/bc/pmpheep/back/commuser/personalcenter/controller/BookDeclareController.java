bf3b868781d2a7b254ccd59bd1b55a69830aaa70
zqf050217 <791038935@qq.com>
2018-02-08 18:27:22 +0800

    1

--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/personalcenter/controller/BookDeclareController.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/personalcenter/controller/BookDeclareController.java
@@ -73,8 +73,8 @@ public class BookDeclareController extends BaseController {
 		Map<String, Object> BankMap = new HashMap<String, Object>();
 		BankMap.put("user_id", request.getParameter("user_id"));
 		BankMap.put("account_name", request.getParameter("realname"));
-		BankMap.put("account_number", request.getParameter("account_number"));
-		BankMap.put("bank", request.getParameter("bank"));
+		BankMap.put("account_number", request.getParameter("bank"));
+		BankMap.put("bank", request.getParameter("account_number"));
 		int bankCount = this.bdecService.insertBank(BankMap);
 		if (bankCount > 0) {
 			List<Map<String, Object>> BankList = new ArrayList<Map<String, Object>>();
