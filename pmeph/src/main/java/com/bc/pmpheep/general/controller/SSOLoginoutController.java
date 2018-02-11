c5557aee8b24bdce377ba4aaf3e51028573813fc
lideveloper <437305629@qq.com>
2018-02-11 16:49:21 +0800

    登录添加积分

--- pmeph/src/main/java/com/bc/pmpheep/general/controller/SSOLoginoutController.java
+++ pmeph/src/main/java/com/bc/pmpheep/general/controller/SSOLoginoutController.java
@@ -2,7 +2,9 @@ package com.bc.pmpheep.general.controller;
 
 import com.bc.pmpheep.back.util.Const;
 import com.bc.pmpheep.general.service.UserService;
+import com.bc.pmpheep.utils.HttpRequestUtil;
 import org.apache.commons.lang.StringUtils;
+import org.apache.struts.util.RequestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -42,7 +45,15 @@ public class SSOLoginoutController {
             session.setAttribute(Const.SESSION_USER_CONST_TYPE, "2");
         }
 
-        if (StringUtils.isEmpty(request.getParameter("refer")) || request.getParameter("refer").endsWith("/")|| request.getParameter("refer").endsWith("tohomepage.action")) {
+        Map<String, Object> params = new HashMap<String, Object>(user);
+
+        params.put("clientip", HttpRequestUtil.getClientIP(request));
+
+        userService.insertUserScores(params);
+        userService.insertUserLoginLog(params);
+
+
+        if (StringUtils.isEmpty(request.getParameter("refer")) || request.getParameter("refer").endsWith("/") || request.getParameter("refer").endsWith("tohomepage.action")) {
             if ("1".equals(usertype)) {
                 response.sendRedirect(request.getContextPath() + "/");
             } else if ("2".equals(usertype)) {
