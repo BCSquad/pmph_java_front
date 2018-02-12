0ae9c545cbfdb7724cddc6e2c730996a1ed569c7
xiaochengyan <985814585@qq.com>
2018-02-12 15:03:03 +0800

    变量错误

--- pmeph/src/main/webapp/resources/commuser/mygroup/group.js
+++ pmeph/src/main/webapp/resources/commuser/mygroup/group.js
@@ -49,7 +49,7 @@ $(function(){
 	};
 	//监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
     window.onbeforeunload = function(){
-        websocket.close();
+    	webSocket.close();
     };
 	webSocket.onmessage = function(event){
 	    //接受来自服务器的消息
