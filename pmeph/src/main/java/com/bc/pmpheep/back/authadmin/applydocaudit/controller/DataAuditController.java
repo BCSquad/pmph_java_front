9256d3ab0fede85a8157376c1f589eb98106a722
xiaochengyan <985814585@qq.com>
2018-03-07 18:20:24 +0800

    注解问题

--- pmeph/src/main/java/com/bc/pmpheep/back/authadmin/applydocaudit/controller/DataAuditController.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/authadmin/applydocaudit/controller/DataAuditController.java
@@ -228,7 +228,7 @@ public class DataAuditController extends BaseController{
 			jcbxList=this.dataAuditService.queryJcbx(queryMap);
 			//人卫社教材编写情况
 			List<Map<String,Object>> rwsList = new ArrayList<Map<String,Object>>();
-			rwsList=this.dataAuditService.queryRwsBook(queryMap);
+			//rwsList=this.dataAuditService.queryRwsBook(queryMap);
 			//11.作家科研情况表
 			List<Map<String,Object>> zjkyList = new ArrayList<Map<String,Object>>();
 			zjkyList = this.dataAuditService.queryZjkyqk(queryMap);
@@ -268,7 +268,7 @@ public class DataAuditController extends BaseController{
 			mav.addObject("workList", workList);
 			mav.addObject("steaList", steaList);
 			mav.addObject("jcbjList", jcbjList);
-			mav.addObject("rwsList",rwsList);
+//			mav.addObject("rwsList",rwsList);
 			mav.addObject("gjkcjsList", gjkcjsList);
 			mav.addObject("jcbxList", jcbxList);
 			mav.addObject("gjghjcList", gjghjcList);
