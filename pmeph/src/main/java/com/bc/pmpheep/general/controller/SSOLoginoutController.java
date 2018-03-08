05ee5f30d81ef51dcc94d3fee6ba6e5c623986b9
lideveloper <437305629@qq.com>
2018-03-08 18:07:48 +0800

    添加内部登录接口

--- pmeph/src/main/java/com/bc/pmpheep/general/controller/SSOLoginoutController.java
+++ pmeph/src/main/java/com/bc/pmpheep/general/controller/SSOLoginoutController.java
@@ -2,20 +2,26 @@ package com.bc.pmpheep.general.controller;
 
 import com.bc.pmpheep.back.interceptor.LoginInterceptor;
 import com.bc.pmpheep.back.util.Const;
+import com.bc.pmpheep.back.util.MD5;
 import com.bc.pmpheep.general.service.UserService;
 import com.bc.pmpheep.utils.HttpRequestUtil;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.struts.util.RequestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -150,5 +156,41 @@ public class SSOLoginoutController {
 
 
     }
+
+
+    @RequestMapping(value = "innerlogin", method = RequestMethod.GET)
+    public void innerLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String value = MD5.md5(username + "1005387596c57c2278f4f61058c78d1b" + new SimpleDateFormat("yyyyMMdd").format(new Date())).toLowerCase();
+        String refer = request.getParameter("refer");
+        if ((StringUtils.isEmpty(request.getParameter("md5")) ? "" : request.getParameter("md5")).toLowerCase().equals(value)) {
+            String usertype = request.getParameter("usertype");
+            Map<String, Object> user = userService.getUserInfo(username, usertype);
+            if (user == null) {
+                response.sendRedirect(request.getContextPath() + "/pages/comm/login.jsp?refer=" + request.getParameter("refer") + "&msg=username is not exist");
+                return;
+            }
+            HttpSession session = request.getSession();
+            if ("1".equals(usertype)) {
+                session.setAttribute(Const.SESSION_USER_CONST_WRITER, user);
+                session.setAttribute(Const.SESSION_USER_CONST_TYPE, "1");
+            } else if ("2".equals(usertype)) {
+                session.setAttribute(Const.SESSION_USER_CONST_ORGUSER, user);
+                session.setAttribute(Const.SESSION_USER_CONST_TYPE, "2");
+            }
+
+
+            if(!StringUtils.isEmpty(refer)){
+                response.sendRedirect(refer);
+            }else if ("1".equals(usertype)) {
+                response.sendRedirect(request.getContextPath() + "/homepage/tohomepage.action");
+            } else if ("2".equals(usertype)) {
+                response.sendRedirect(request.getContextPath() + "/schedule/scheduleList.action");
+            }
+        } else {
+            response.setStatus(500);
+        }
+    }
+
 
 }
