43423d126b6469113c731697d4fa709dbd920933
lideveloper <437305629@qq.com>
2018-03-20 11:08:35 +0800

    bug:1525    上传视频出错     限制为20个字符

--- pmeph/src/main/webapp/resources/commuser/readpage/readdetail.js
+++ pmeph/src/main/webapp/resources/commuser/readpage/readdetail.js
@@ -218,11 +218,17 @@ $(function () {
                                             add: function (e, data) {
 
                                                 var valid = function () {
-                                                    if ($(".pop-body").find("input[type='text']").val()) {
+                                                    if ($(".pop-body").find("input[type='text']").val()
+                                                        && $(".pop-body").find("input[type='text']").val().length <= 20) {
+
                                                         $(".pop-body").find("button.submit").removeClass("disable");
                                                         return true;
                                                     } else {
                                                         $(".pop-body").find("button.submit").addClass("disable");
+                                                        if ($(".pop-body").find("input[type='text']").val()
+                                                            && $(".pop-body").find("input[type='text']").val().length > 20) {
+                                                            message.warning("标题长度不能超过20个字符");
+                                                        }
                                                         return false;
                                                     }
                                                 }
