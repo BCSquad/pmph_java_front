66fa64776e323a21766aa1d5f2e6aca67054d39e
liudi-aditenmin <swadia_liudi@163.com>
2018-04-27 11:57:29 +0800

    2706 N146-审核教师和机构用户时，退出添加退回原因，前台需要显示退出原因
    教师认证增加退回原因

--- pmeph/src/main/java/com/bc/pmpheep/back/authadmin/teacherauth/controller/TeacherauthController.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/authadmin/teacherauth/controller/TeacherauthController.java
@@ -100,9 +100,10 @@ public class TeacherauthController extends BaseController {
     public Map<String, Object> statusModify(HttpServletRequest request) {
         String status = request.getParameter("status");
         String id = request.getParameter("id");
+        String backReason = request.getParameter("backReason");
         Map<String, Object> userInfo = getUserInfo();
 
-        Map<String, Object> rb = teacherAuthService.statusModify(id, status, MapUtils.getString(userInfo, "org_id"));
+        Map<String, Object> rb = teacherAuthService.statusModify(id, status, backReason,MapUtils.getString(userInfo, "org_id"));
 
         return rb;
     }
