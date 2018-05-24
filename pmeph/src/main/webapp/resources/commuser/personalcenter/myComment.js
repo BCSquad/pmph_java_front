9ed63b017346445885277b0ed44dda09a7f16f64
huangqiongfei <591826445@qq.com>
2018-05-24 14:50:59 +0800

    bug修改

--- pmeph/src/main/webapp/resources/commuser/personalcenter/myComment.js
+++ pmeph/src/main/webapp/resources/commuser/personalcenter/myComment.js
@@ -208,7 +208,7 @@ function LengthLimit(obj,ml){
 	for ( var i = 1; i <= va.length; i++) {
 		vat = va.substring(0,i);
 		//把双字节的替换成两个单字节的然后再获得长度，与限制比较
-		if (vat.replace(/[^\x00-\xff]/g,"aa").length <= ml) {
+		if (vat.replace(/[^\x00-\xff]/g,"a").length <= ml) {
 			maxStrlength=i;
 		}else{
 			
@@ -217,7 +217,7 @@ function LengthLimit(obj,ml){
 	}
 	obj.maxlength=maxStrlength;
 	//把双字节的替换成两个单字节的然后再获得长度，与限制比较
-	if (va.replace(/[^\x00-\xff]/g,"aa").length > ml) {
+	if (va.replace(/[^\x00-\xff]/g,"a").length > ml) {
 		obj.value=va.substring(0,maxStrlength);
 		window.message.warning("不可超过输入最大长度"+ml+"字节！");
 	}
