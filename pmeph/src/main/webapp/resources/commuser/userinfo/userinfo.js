15ca5675c32b46f49cde85a6e27b75dcd9ea37f6
liudi-aditenmin <swadia_liudi@163.com>
2018-06-05 11:30:26 +0800

    2808 个人资料修改
    建议把字节改为字，如果只能输入10个字。提示 ： 不可超过输入最大长度10字！

--- pmeph/src/main/webapp/resources/commuser/userinfo/userinfo.js
+++ pmeph/src/main/webapp/resources/commuser/userinfo/userinfo.js
@@ -147,7 +147,7 @@ function LengthLimit(obj, ml) {
     for (var i = 1; i <= va.length; i++) {
         vat = va.substring(0, i);
         //把双字节的替换成两个单字节的然后再获得长度，与限制比较
-        if (vat.replace(/[^\x00-\xff]/g, "aa").length <= ml) {
+        if (vat.replace(/[^\x00-\xff]/g, "a").length <= ml) {
            var maxStrlength = i;
         } else {
 
@@ -156,9 +156,9 @@ function LengthLimit(obj, ml) {
     }
     obj.maxlength = maxStrlength;
     //把双字节的替换成两个单字节的然后再获得长度，与限制比较
-    if (va.replace(/[^\x00-\xff]/g, "aa").length > ml) {
+    if (va.replace(/[^\x00-\xff]/g, "a").length > ml) {
         obj.value = va.substring(0, maxStrlength);
-        window.message.warning("不可超过输入最大长度" + ml + "字节！");
+        window.message.warning("不可超过输入最大长度" + ml + "字！");
     }
 }
 
