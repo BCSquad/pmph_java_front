04c7c3caac96047ba64fda5ca36aedcaed0b62b7
huangqiongfei <591826445@qq.com>
2018-06-25 08:39:50 +0800

    自动下拉加载

--- pmeph/src/main/webapp/resources/comm/base.js
+++ pmeph/src/main/webapp/resources/comm/base.js
@@ -437,7 +437,42 @@ function Empty(v) {
     return false;
 }
 
+/**鼠标下拉自动加载**/
+//获取滚动条当前的位置
+function getScrollTop() {
+    var scrollTop = 0;
+    if(document.documentElement && document.documentElement.scrollTop) {
+        scrollTop = document.documentElement.scrollTop;
+    } else if(document.body) {
+        scrollTop = document.body.scrollTop;
+    }
+    return scrollTop;
+}
 
+//获取当前可视范围的高度
+function getClientHeight() {
+    var clientHeight = 0;
+    if(document.body.clientHeight && document.documentElement.clientHeight) {
+        clientHeight = Math.min(document.body.clientHeight, document.documentElement.clientHeight);
+    } else {
+        clientHeight = Math.max(document.body.clientHeight, document.documentElement.clientHeight);
+    }
+    return clientHeight;
+}
+
+//获取文档完整的高度
+function getScrollHeight() {
+    return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+}
+
+window.onscroll = function() {
+
+    if(getScrollTop() + getClientHeight()+50 > getScrollHeight()) {
+        console.log('下拉刷新了')
+        //此处发起AJAX请求
+        loadData();
+    }
+}
 
 
 
