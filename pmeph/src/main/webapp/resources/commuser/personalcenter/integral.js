b4964947d08839aff3161df08f62fcf218025c2d
huangqiongfei <591826445@qq.com>
2018-07-16 18:04:14 +0800

    修改bug

--- pmeph/src/main/webapp/resources/commuser/personalcenter/integral.js
+++ pmeph/src/main/webapp/resources/commuser/personalcenter/integral.js
@@ -1,3 +1,8 @@
+/* 初始化 */
+var counter = 0; /* 计数器 */
+var pageStart = 0; /* offset */
+var pageSize = 3; /* size */
+
 $(function() {
     	//积分筛选
         $('#sele').selectlist({
@@ -16,10 +21,6 @@ $(function() {
         	queryTime();
     	});
         
-	/* 初始化 */
-	var counter = 0; /* 计数器 */
-	var pageStart = 0; /* offset */
-	var pageSize = 10; /* size */
 
 	/* 首次加载 最终名单初始化 */
 	getData(pageStart, pageSize);
@@ -150,6 +151,9 @@ function formatDate(nS,str) {
 /**
  * 加载更多
  */
+
  function  loadData() {
+     counter++;
+     pageStart = counter * pageSize;
 	 getData(pageStart, pageSize);
 }
