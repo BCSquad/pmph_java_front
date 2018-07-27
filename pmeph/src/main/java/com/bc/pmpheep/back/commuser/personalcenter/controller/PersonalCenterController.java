57cdc576dc54c5520585fce0ec06d2e62b02cc07
mingbaobei <1258441695@qq.com>
2018-07-27 15:47:07 +0800

    微调

--- pmeph/src/main/java/com/bc/pmpheep/back/commuser/personalcenter/controller/PersonalCenterController.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/commuser/personalcenter/controller/PersonalCenterController.java
@@ -779,11 +779,5 @@ public class PersonalCenterController extends BaseController {
 		return map1;
 	}
 
-	@RequestMapping(value="/declare")
-	public ModelAndView topz(){
-		ModelAndView modelAndView=new ModelAndView();
-		modelAndView.setViewName("commuser/personalcenter/declare");
-		return modelAndView;
-	}
 
 }
