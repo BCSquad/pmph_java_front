0562e0ef6c41c4de049ee9893f049490cb49e117
kuso <6516536767@qq.com>
2020-06-23 10:51:10 +0800

    修改导出,及首页信息快报排序

--- pmeph/src/main/java/com/bc/pmpheep/back/authadmin/applydocaudit/service/DeclareResultExcelService.java
+++ pmeph/src/main/java/com/bc/pmpheep/back/authadmin/applydocaudit/service/DeclareResultExcelService.java
@@ -1,10 +1,9 @@
 package com.bc.pmpheep.back.authadmin.applydocaudit.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import com.bc.pmpheep.back.authadmin.applydocaudit.dao.DataAuditDao;
+import com.bc.pmpheep.back.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +23,8 @@ public class DeclareResultExcelService implements ExcelDownloadService {
 	
 	@Autowired
 	DeclareCountDao declareCountDao;
+    @Autowired
+    DataAuditDao dataAuditDao;
 	
 	
     @Override
@@ -45,8 +46,17 @@ public class DeclareResultExcelService implements ExcelDownloadService {
     public List<Map<String, Object>> getData(Map<String, Object> param) throws Exception {
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
         
-        List<Map<String, Object>> resultList = declareCountDao.selectResults(param);
-        
+        List<Map<String, Object>> resultList = new ArrayList<>();
+         String materialCreateDate = dataAuditDao.findMaterialCreateDate(param);
+        Date date1 = DateUtil.fomatDate(materialCreateDate);
+        Date date = DateUtil.fomatDate("2019-04-12 12:00");
+        /*Date date = DateUtil.fomatDate("2019-4-19 17:00");*/
+        if(date1.getTime()>date.getTime()) {
+            resultList=declareCountDao.selectResults2(param);
+        }else{
+            resultList=declareCountDao.selectResults(param);
+
+        }
         for (int i = 0; i < resultList.size(); i++) {
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("textbook_name", resultList.get(i).get("textbook_name"));
